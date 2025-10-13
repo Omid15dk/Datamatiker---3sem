4d7216e0fb126d9a14879676d2c560fe9b1e88dd
Omid15dk <10632132@ucn.dk>
2025-10-13 13:44:04 +0200

    Created a frontend exercise with HTML, CSS & js

---
+++ WebStarterExercise/JavaScript/general.js
@@ -0,0 +1,9 @@
+// scripts/main.js
+
+window.onload = function () {
+    // Show an alert box
+    alert("Page is loaded");
+
+    // Change the text inside the <article> tag
+    document.querySelector("article").innerText = "JavaScript did this..";
+};
